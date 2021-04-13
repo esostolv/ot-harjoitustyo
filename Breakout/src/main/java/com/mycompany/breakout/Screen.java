51217e475d68286d3d8278355f1fa3089d59b35e
oskarito <oskarito@ad.helsinki.fi>
2021-04-13 20:49:55 +0300

    Luotu luokat ohjelmaa varten

--- Breakout/src/main/java/com/mycompany/breakout/Screen.java
+++ Breakout/src/main/java/com/mycompany/breakout/Screen.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.mycompany.breakout;
 
-/**
- *
- * @author Oskari
- */
+package com.mycompany.breakout;
+import javafx.application.Application;
+
 public class Screen {
+    
+    public void start() {
+        
+    }
     
 }
